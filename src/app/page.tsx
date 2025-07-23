9c7d19264d0fe211d3e69cd0230c5e08de96a897
Tobias Fechner <tobias.fechner@hotmail.co.uk>
2025-07-24 00:47:21 +0200

    address ui bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 
 const stakeholderTypes = [
   { name: 'Participants', id: '3NB7ON', description: 'Individuals', image: '/card-participant.jpg' },
-  { name: 'Communities of Place', id: 'w4NK1O', description: 'Bioregional hubs & locals', image: '/card-copl.jpg' },
+  { name: 'Communities of Place', id: 'w4NK1O', description: 'Regional hubs and locals', image: '/card-copl.jpg' },
   { name: 'Communities of Practice', id: 'n9J7rK', description: 'Network representatives', image: '/card-copr.jpg' },
   { name: 'Partners', id: 'wQB7El', description: 'Technologies, funders, institutions', image: '/card-partners.jpg' },
 ];
@@ -24,15 +24,15 @@ export default function Home() {
       <div className="min-h-screen flex flex-col items-center justify-center relative z-20 pt-5 pb-5">
         <a href="https://www.wada.org" target="_blank" rel="noopener noreferrer" className="mb-6">
           <Image 
-              src="/wada_brand_assets/Wada-RGB_Logo-Full-Alternative-Color.svg"
+              src="/brand_assets/Wada-RGB_Logo-Full-Alternative-Color.svg"
               alt="Wada Logo" 
               className="h-16 w-auto" 
               width={64} // Specify width
               height={64} // Specify height
           />
         </a>
-        <div className="container mx-auto px-16">
-          <h1 className="text-4xl font-custom font-extrabold mb-8 text-center text-white">Choose Your Registration Path</h1>
+        <div className="container mx-auto px-16 mb-6">
+          <h1 className="text-4xl font-custom font-extrabold text-center text-white">Choose Your Registration Path</h1>
         </div>
         <div className="container mx-auto px-16">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
@@ -40,7 +40,7 @@ export default function Home() {
               <div
                 key={type.id}
                 onClick={() => handleCardClick(type.id)} // Call handleCardClick on click
-                className="relative bg-gray-800 bg-opacity-40 rounded-xl border border-gray-600 shadow-lg overflow-hidden hover:shadow-xl duration-300 cursor-pointer flex flex-col transform transition-transform hover:scale-105"
+                className="relative hover:bg-wada-b/30 rounded-xl border border-gray-600 shadow-lg overflow-hidden duration-500 cursor-pointer flex flex-col hover:scale-105"
               >
                 <Image
                   src={type.image}
@@ -49,11 +49,11 @@ export default function Home() {
                   width={200} 
                   height={150}
                 />
-                <div className="h-0.5 bg-white bg-opacity-30" />
-                <div className="absolute inset-0 bg-gray-600 bg-opacity-15 z-10" />
+                <div className="h-0.5 bg-gray-600" />
+                <div className="absolute inset-0 -z-10" />
                 <div className="p-6">
-                  <h2 className="text-2xl font-semibold text-white mb-2 z-20 relative truncate">{type.name}</h2>
-                  <p className="text-gray-300 mb-4 truncate">{type.description}</p>
+                  <h2 className="md:text-2xl font-custom text-white mb-2 z-20 relative truncate">{type.name}</h2>
+                  <p className="font-custom text-gray-300 mb-4 truncate">{type.description}</p>
                 </div>
               </div>
             ))}
