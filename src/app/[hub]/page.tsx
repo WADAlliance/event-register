4e19ba00987367ab2b339a4ae2e98476a7de893f
tbsfchnr <34064065+tbsfchnr@users.noreply.github.com>
2025-07-27 23:28:18 +0100

    Update page.tsx

--- src/app/[hub]/page.tsx
+++ src/app/[hub]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import { useRouter } from 'next/navigation';
 import { motion } from 'motion/react';
 import { FaChevronDown } from "react-icons/fa6";
