c7c544fc18ed84050f38959ecd7a6e896cf8e298
Tobias Fechner <tobias.fechner@hotmail.co.uk>
2025-09-11 19:30:55 +0200

    change vids

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import StakeholderCardGrid from './components/StakeholderCardGrid';
 
 const stakeholderTypes = [
   { name: 'Participants', id: 'mD627j', description: 'Individuals', video: '/videos/cats_dinner.mp4' },
-  { name: 'Communities of Place', id: 'w4NK1O', description: 'Regional hubs and locals', video: '/videos/cats_dinner.mp4' },
-  { name: 'Communities of Practice', id: 'n9J7rK', description: 'Network representatives', video: '/videos/cats_dinner.mp4' },
-  { name: 'Partners', id: 'wQB7El', description: 'Technologies, funders, institutions', video: '/videos/cats_dinner.mp4' },
+  { name: 'Communities of Place', id: 'w4NK1O', description: 'Regional hubs and locals', video: '/videos/boat_selfie.mp4' },
+  { name: 'Communities of Practice', id: 'n9J7rK', description: 'Network representatives', video: '/videos/cats_fire.mp4' },
+  { name: 'Partners', id: 'wQB7El', description: 'Technologies, funders, institutions', video: '/videos/cats_dance.mp4' },
 ];
 
 export default function Home() {
