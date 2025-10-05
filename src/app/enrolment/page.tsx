0b7a48f518dd62b966febf1f92c044ceee086ac2
Tobias Fechner <tobias.fechner@hotmail.co.uk>
2025-10-06 00:34:21 +0200

    amend extra info on roles cards

--- src/app/enrolment/page.tsx
+++ src/app/enrolment/page.tsx
@@ -6,7 +6,7 @@ import StakeholderCardGrid from '@/components/StakeholderCardGrid';
 
 const stakeholderTypes = [
   { name: 'Participants', id: 'mD627j', description: 'Individuals', video: '/videos/cats_dinner.mp4', extraInfo: "Participants are all individuals including developers, artists, farmers, community leaders, and anyone else wanting to hack on local systems.", disabled: false },
-  { name: 'Communities of Place', id: 'w4NK1O', description: 'Regional hubs and locals', video: '/videos/boat_selfie.mp4', extraInfo: "# Place\nPlace is your neighborhood, your community, your region. # Hubs\nHubs are the centres of place.", disabled: false },
+  { name: 'Communities of Place', id: 'w4NK1O', description: 'Regional hubs and locals', video: '/videos/boat_selfie.mp4', extraInfo: "# Place\nPlace is your neighborhood, your community, your region, including hubs - the centres of place.", disabled: false },
   { name: 'Communities of Practice', id: 'n9J7rK', description: 'Network representatives', video: '/videos/cats_fire.mp4', extraInfo: "# Role\nThis stakeholder is interested in how to best *collaborate*. \n\n# Profile Description\nProbably you are someone who \n- fits this kind of \n- archetype because if youre this archetype then youre this role innit.", disabled: true },
   { name: 'Partners', id: 'wQB7El', description: 'Technologies, funders, institutions', video: '/videos/cats_dance.mp4', extraInfo: "", disabled: true },
 ];
