cd6b8826c677da778f6de87b0ce3ae4e2b10ff4b
Daniel Mwihoti <danielmwihoti@gmail.com>
2025-11-11 16:53:23 +0000

    fix(active-menu)- copy previous active-menu branch into this

---
+++ src/app/partners/page.tsx
@@ -0,0 +1,9 @@
+import PartnersSection from "@/components/landing/PartnersSection";
+
+export default function PartnersSectionPage() {
+  return (
+    <main>
+      <PartnersSection />
+    </main>
+  );
+}
---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,9 @@
+import SummitSchedule from "@/components/landing/SummitSchedule";
+
+export default function SummitSchedulePage() {
+  return (
+    <main>
+      <SummitSchedule />
+    </main>
+  );
+}
