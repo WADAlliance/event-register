06d47e4874ffb6304ba421e00d92e6ea664c3a20
Tobias Fechner <tobias.fechner@hotmail.co.uk>
2025-12-28 14:16:37 +0000

    update forms IDs to forms used for teams to update their registrations

--- src/app/enrolment/page.tsx
+++ src/app/enrolment/page.tsx
@@ -6,11 +6,11 @@ import '@/styles/globals.css';
 import StakeholderCardGrid from '@/components/StakeholderCardGrid';
 
 const stakeholderTypes = [
-  { name: 'Participants', id: 'b5q601', description: 'Team Formations', video: '/videos/cats_dinner.mp4', extraInfo: "Participants are now invited to register their teams. Please submit only one form per-team. You do not need to submit for every team member.", disabled: false },
+  { name: 'Participants', id: 'eqDVMl', description: 'Team Formations', video: '/videos/cats_dinner.mp4', extraInfo: "Participants are now invited to update their teams, if suitable.", disabled: false },
   { name: 'Communities of Place', id: 'GxKv6k', description: 'Hub story publishing', video: '/videos/boat_selfie.mp4', extraInfo: "Hubs are now invited to publish their first curation.", disabled: false },
   { name: 'Impact Networks', id: 'n9J7rK', description: 'Network representatives', video: '/videos/cats_fire.mp4', extraInfo: "# Role\nThis stakeholder is interested in how to best *collaborate*. \n\n# Profile Description\nProbably you are someone who \n- fits this kind of \n- archetype because if youre this archetype then youre this role innit.", disabled: true },
   { name: 'Partners', id: 'wQB7El', description: 'Technologies, funders, institutions', video: '/videos/cats_dance.mp4', extraInfo: "", disabled: true },
-  { name: 'Participants', id: '5B420o', description: 'Formations d’équipes', video: '/videos/cats_dinner.mp4', extraInfo: "Les participants sont désormais invités à enregistrer leurs équipes. Veuillez soumettre un seul formulaire par équipe. Il n’est pas nécessaire de remplir un formulaire pour chaque membre de l’équipe.", disabled: false },
+  { name: 'Participants', id: 'xXjA2G', description: 'Formations d’équipes', video: '/videos/cats_dinner.mp4', extraInfo: "Les participants sont désormais invités à mettre à jour les inscriptions de leur équipe.", disabled: false },
   { name: 'Communautés territoriales', id: 'QKM2Np', description: 'Publication des récits du hub', video: '/videos/boat_selfie.mp4', extraInfo: "Les hubs sont désormais invités à publier leur première curation.", disabled: false },
 ];
 
@@ -23,7 +23,7 @@ export default function Home() {
   }
 
   // Filter stakeholder types based on language toggle
-  const frenchIds = ['5B420o', 'QKM2Np'];
+  const frenchIds = ['xXjA2G', 'QKM2Np'];
 
   const filteredStakeholderTypes = isFrench 
     ? stakeholderTypes.filter(type => frenchIds.includes(type.id)) // Only show French version
