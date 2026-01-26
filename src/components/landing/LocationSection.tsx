e34f6a66b3c637f68d26ff5e7604d89e9571410f
Baraka <baraka@Barakas-MacBook-Air.local>
2026-01-26 13:11:00 +0300

    feat: adding speakers

--- src/components/landing/LocationSection.tsx
+++ src/components/landing/LocationSection.tsx
@@ -55,13 +55,13 @@ export default function LocationSection() {
                         <div className="self-stretch flex flex-col justify-start items-start gap-5">
                             <VenueMap className="self-stretch h-52 sm:h-64 lg:h-90 rounded-[10px]" mapSrc='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8521203860246!2d36.79944617534973!3d-1.2609658987270305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f176b6e9bd5e5%3A0xcd8fcd4811d8fc0f!2sThe%20Sarit%20Expo%20Centre!5e0!3m2!1sen!2sus!4v1762879826139!5m2!1sen!2sus' />
                             <div className="self-stretch inline-flex justify-center items-center gap-3.5">
-                                <div className="px-5 py-2.5 bg-wada-c rounded-md flex justify-center items-center gap-2.5 whitespace-nowrap h-10">
-                                    <RegisterForHackathonButton className="font-telegraf font-extrabold text-white duration-300 whitespace-nowrap" text="Register for the Hackathon" />
-                                </div>
+                                {/*<div className="px-5 py-2.5 bg-wada-c rounded-md flex justify-center items-center gap-2.5 whitespace-nowrap h-10">*/}
+                                {/*    <RegisterForHackathonButton className="font-telegraf font-extrabold text-white duration-300 whitespace-nowrap" text="Register for the Hackathon" />*/}
+                                {/*</div>*/}
                             </div>
                             <div className="self-stretch h-0 outline-1 outline-offset-[-0.50px] outline-black/10"></div>
-                            <div className="self-stretch flex flex-col sm:flex-row justify-center items-center gap-3">
-                                <div className="flex-1 inline-flex flex-col justify-center items-center text-center max-w-[640px]">
+                            <div className="self-stretch flex flex-col sm:flex-row justify-center items-center gap-3 mt-auto">
+                                <div className="flex-1 inline-flex flex-col justify-center items-center text-center max-w-160 pt-5">
                                     <div className="px-2.5 inline-flex justify-center items-center gap-2.5">
                                         <div className="text-center text-black group-hover:text-white text-base sm:text-lg font-extrabold font-telegraf leading-5 sm:leading-6 transition-colors duration-300">Open to developers and professionals</div>
                                     </div>
@@ -69,12 +69,12 @@ export default function LocationSection() {
                                         <div className="text-center text-black group-hover:text-white text-xs sm:text-sm font-poppins leading-6 sm:leading-7 transition-colors duration-300">Prizes, mentorship & incubation opportunities</div>
                                     </div>
                                 </div>
-                                <div className="w-full sm:w-auto flex justify-center">
-                                    <div className="px-4 sm:px-5 py-2.5 sm:py-3.5 bg-wada-c rounded-[32px] flex justify-center items-center gap-2.5 whitespace-nowrap">
-                                        <div className="w-2.5 h-2.5 bg-cardano-c rounded-full"></div>
-                                        <div className="text-center text-white text-xs sm:text-sm font-extrabold font-telegraf">Registration Ongoing</div>
-                                    </div>
-                                </div>
+                                {/*<div className="w-full sm:w-auto flex justify-center">*/}
+                                {/*    <div className="px-4 sm:px-5 py-2.5 sm:py-3.5 bg-wada-c rounded-[32px] flex justify-center items-center gap-2.5 whitespace-nowrap">*/}
+                                {/*        <div className="w-2.5 h-2.5 bg-cardano-c rounded-full"></div>*/}
+                                {/*        <div className="text-center text-white text-xs sm:text-sm font-extrabold font-telegraf">Registration Ongoing</div>*/}
+                                {/*    </div>*/}
+                                {/*</div>*/}
                             </div>
                         </div>
                     </div>
@@ -106,29 +106,33 @@ export default function LocationSection() {
                             <div className="self-stretch inline-flex justify-center items-center gap-3.5">
                                 <div className="px-5 py-3.5 bg-cardano-c rounded-md flex justify-start items-center gap-2.5">
                                     <Link
-                                        href="/summit"
+                                        href="/trip-planner"
                                         className="font-telegraf font-extrabold text-white duration-300"
                                     >
-                                        Get Summit Pass
+                                        Plan My Trip
                                     </Link>
                                 </div>
                             </div>
-                            <div className="self-stretch h-0 outline outline-1 outline-offset-[-0.50px] outline-black/10"></div>
+                            <div className="self-stretch h-0 outline-1 outline-offset-[-0.50px] outline-black/10"></div>
                             <div className="self-stretch flex flex-col sm:flex-row justify-start items-start sm:items-center gap-3">
-                                <div className="flex-1 inline-flex flex-col justify-center items-start">
-                                    <div className="px-2.5 inline-flex justify-start items-center gap-2.5">
-                                        <div className="text-left justify-start text-black group-hover:text-white text-base sm:text-lg font-extrabold font-telegraf leading-5 sm:leading-6 transition-colors duration-300">Speakers • Panels • Networking</div>
+                                <div className="flex-1 inline-flex flex-col justify-start items-center w-full">
+                                    <div className="px-2.5 flex justify-center items-center gap-2.5 w-full">
+                                        <div className="text-left justify-start text-black group-hover:text-white text-base sm:text-lg font-extrabold font-telegraf leading-5 sm:leading-6 transition-colors duration-300">
+                                            Speakers • Panels • Networking
+                                        </div>
                                     </div>
-                                    <div className="self-stretch px-2.5 inline-flex justify-center items-center gap-2.5">
-                                        <div className="flex-1 text-center text-black group-hover:text-white text-xs sm:text-sm font-poppins leading-6 sm:leading-7 transition-colors duration-300">Industry leaders & ecosystem<br />founders</div>
-                                    </div>
-                                </div>
-                                <div className="w-full sm:w-auto flex justify-center">
-                                    <div className="px-4 sm:px-5 py-2.5 sm:py-3.5 bg-black group-hover:bg-gray-600 rounded-[32px] flex justify-center items-center gap-2.5 whitespace-nowrap transition-colors duration-300">
-                                        <div className="w-2.5 h-2.5 bg-cardano-c rounded-full"></div>
-                                        <div className="text-center text-white text-xs sm:text-sm font-extrabold font-telegraf">Registration Ongoing</div>
+                                    <div className="self-stretch px-2.5 inline-flex justify-center items-center gap-2.5 w-full">
+                                        <div className="flex-1 text-center text-black group-hover:text-white text-xs sm:text-sm font-poppins leading-6 sm:leading-7 transition-colors duration-300">
+                                            Industry leaders & ecosystem founders
+                                        </div>
                                     </div>
                                 </div>
+                                {/*<div className="w-full sm:w-auto flex justify-center">*/}
+                                {/*    <div className="px-4 sm:px-5 py-2.5 sm:py-3.5 bg-black group-hover:bg-gray-600 rounded-[32px] flex justify-center items-center gap-2.5 whitespace-nowrap transition-colors duration-300">*/}
+                                {/*        <div className="w-2.5 h-2.5 bg-cardano-c rounded-full"></div>*/}
+                                {/*        <div className="text-center text-white text-xs sm:text-sm font-extrabold font-telegraf">Registration Ongoing</div>*/}
+                                {/*    </div>*/}
+                                {/*</div>*/}
                             </div>
                         </div>
                     </div>
